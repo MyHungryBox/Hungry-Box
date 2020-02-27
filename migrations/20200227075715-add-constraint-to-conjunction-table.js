2cb7a02d5b3a2ff89c1b86a8857a96d2cea651ae
hamzah <primoakunta@gmail.com>
2020-02-27 15:02:25 +0700

    add constraint

---
+++ migrations/20200227075715-add-constraint-to-conjunction-table.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+   return queryInterface.addConstraint('MenuUsers', ['MenuId'], {
+    type: 'foreign key',
+    name: 'custom_fkey_MenuId',
+    references: { //Required field
+      table: 'Menus',
+      field: 'id'
+    },
+    onDelete: 'cascade',
+    onUpdate: 'cascade'
+  });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+   return queryInterface.removeConstraint('MenuUsers', 'custom_fkey_MenuId')
+  }
+};
